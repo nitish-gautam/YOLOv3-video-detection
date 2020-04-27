2ee331ebffe3b238206327d32efacbfdd724fc91
Nitish <46977634+nitish-gautam@users.noreply.github.com>
2020-04-27 20:57:50 +0100

    Update yolov3-camera-realtime-detection.py

--- yolov3-camera-realtime-detection.py
+++ yolov3-camera-realtime-detection.py
@@ -32,16 +32,14 @@ print (cv2.__version__)
 Start of: Reading stream video from camera
 """
 
-# Defining 'VideoCapture' object
-# and reading stream video from camera
+# Defining 'VideoCapture' object and reading stream video from camera
 camera = cv2.VideoCapture(0)
 
 # Preparing variables for spatial dimensions of the frames
 h, w = None, None
 
 """
-End of:
-Reading stream video from camera
+End of: Reading stream video from camera
 """
 
 
@@ -51,27 +49,21 @@ Start of: Loading YOLO v3 network
 """
 
 # Loading COCO class labels from file
-# Opening file
-# Pay attention! If you're using Windows, yours path might looks like:
 with open('yolo-coco-data/coco.names') as f:
     # Getting labels reading every line
     # and putting them into the list
     labels = [line.strip() for line in f]
 
-
-# # Check point
 # print('List with labels names:')
 # print(labels)
 
-# Loading trained YOLO v3 Objects Detector
-# with the help of 'dnn' library from OpenCV
+# Loading trained YOLO v3 Objects Detector with the help of 'dnn' library from OpenCV
 network = cv2.dnn.readNetFromDarknet('yolo-coco-data/yolov3.cfg',
                                      'yolo-coco-data/yolov3.weights')
 
 # Getting list with names of all layers from YOLO v3 network
 layers_names_all = network.getLayerNames()
 
-# # Check point
 # print()
 # print(layers_names_all)
 
@@ -80,15 +72,13 @@ layers_names_all = network.getLayerNames()
 layers_names_output = \
     [layers_names_all[i[0] - 1] for i in network.getUnconnectedOutLayers()]
 
-# # Check point
 # print()
 # print(layers_names_output)  # ['yolo_82', 'yolo_94', 'yolo_106']
 
 # Setting minimum probability to eliminate weak predictions
 probability_minimum = 0.5
 
-# Setting threshold for filtering weak bounding boxes
-# with non-maximum suppression
+# Setting threshold for filtering weak bounding boxes with non-maximum suppression
 threshold = 0.3
 
 # Generating colours for representing every detected object
@@ -116,8 +106,7 @@ while True:
     # Capturing frame-by-frame from camera
     _, frame = camera.read()
 
-    # Getting spatial dimensions of the frame
-    # we do it only once from the very beginning
+    # Getting spatial dimensions of the frame we do it only once from the very beginning
     # all other frames have the same dimension
     if w is None or h is None:
         # Slicing from tuple only first two elements
@@ -132,14 +121,13 @@ while True:
     # The 'cv2.dnn.blobFromImage' function returns 4-dimensional blob from current
     # frame after mean subtraction, normalizing, and RB channels swapping
     # Resulted shape has number of frames, number of channels, width and height
-    # E.G.:
+    # eg.:
     # blob = cv2.dnn.blobFromImage(image, scalefactor=1.0, size, mean, swapRB=True)
     blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
                                  swapRB=True, crop=False)
 
     """
-    End of:
-    Getting blob from current frame
+    End of: Getting blob from current frame
     """
 
     """
@@ -167,8 +155,7 @@ while True:
     Start of: Getting bounding boxes
     """
 
-    # Preparing lists for detected bounding boxes,
-    # obtained confidences and class's number
+    # Preparing lists for detected bounding boxes, obtained confidences and class's number
     bounding_boxes = []
     confidences = []
     classIDs = []
@@ -183,8 +170,7 @@ while True:
             class_current = np.argmax(scores)
             # Getting value of probability for defined class
             confidence_current = scores[class_current]
-
-            # # Check point
+            
             # # Every 'detected_objects' numpy array has first 4 numbers with
             # # bounding box coordinates and rest 80 with probabilities
             # # for every class
@@ -201,21 +187,18 @@ while True:
                 # of bounding box, its width and height for original frame
                 box_current = detected_objects[0:4] * np.array([w, h, w, h])
 
-                # Now, from YOLO data format, we can get top left corner coordinates
-                # that are x_min and y_min
+                # Now, from YOLO data format, we can get top left corner coordinates that are x_min and y_min
                 x_center, y_center, box_width, box_height = box_current
                 x_min = int(x_center - (box_width / 2))
                 y_min = int(y_center - (box_height / 2))
 
                 # Adding results into prepared lists
-                bounding_boxes.append([x_min, y_min,
-                                       int(box_width), int(box_height)])
+                bounding_boxes.append([x_min, y_min, int(box_width), int(box_height)])
                 confidences.append(float(confidence_current))
                 classIDs.append(class_current)
 
     """
-    End of:
-    Getting bounding boxes
+    End of: Getting bounding boxes
     """
 
     """
@@ -235,8 +218,7 @@ while True:
                                probability_minimum, threshold)
 
     """
-    End of:
-    Non-maximum suppression
+    End of: Non-maximum suppression
     """
 
     """
@@ -249,16 +231,13 @@ while True:
     if len(results) > 0:
         # Going through indexes of results
         for i in results.flatten():
-            # Getting current bounding box coordinates,
-            # its width and height
+            # Getting current bounding box coordinates, its width and height
             x_min, y_min = bounding_boxes[i][0], bounding_boxes[i][1]
             box_width, box_height = bounding_boxes[i][2], bounding_boxes[i][3]
 
-            # Preparing colour for current bounding box
-            # and converting from numpy array to list
+            # Preparing colour for current bounding box and converting from numpy array to list
             colour_box_current = colours[classIDs[i]].tolist()
-
-            # # # Check point
+            
             # print(type(colour_box_current))  # <class 'list'>
             # print(colour_box_current)  # [172 , 10, 127]
 
